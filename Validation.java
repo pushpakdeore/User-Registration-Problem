97f30221930bad525466212687dd643eb531990c
pushpakdeore <deorepushpakk@gmail.com>
2024-10-04 14:59:08 +0530

    OK4

--- Validation.java
+++ Validation.java
@@ -8,6 +8,7 @@ public class Validation {
         System.out.print("Enter your first name: ");
         String firstName = scanner.nextLine();
 
+
         if (isValidFirstName(firstName)) {
             System.out.println("Valid first name.");
         } else {
@@ -17,25 +18,41 @@ public class Validation {
         System.out.print("Enter your last name: ");
         String lastName = scanner.nextLine();
 
+
         if (isValidLastName(lastName)) {
             System.out.println("Valid last name.");
         } else {
             System.out.println("Invalid last name. It must start with a capital letter and have at least 3 characters.");
         }
 
+
+        System.out.print("Enter your email: ");
+        String email = scanner.nextLine();
+
+
+        if (isValidEmail(email)) {
+            System.out.println("Valid email.");
+        } else {
+            System.out.println("Invalid email. It must follow the format abc.xyz@bl.co.in.");
+        }
+
         scanner.close();
     }
 
-    public static boolean isValidFirstName(String firstName) {
 
+    public static boolean isValidFirstName(String firstName) {
         String regex = "^[A-Z][a-z]{2,}$";
         return firstName.matches(regex);
     }
 
-
     public static boolean isValidLastName(String lastName) {
         String regex = "^[A-Z][a-z]{2,}$";
         return lastName.matches(regex);
+    }
+
+    public static boolean isValidEmail(String email) {
+        String regex = "^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
+        return email.matches(regex);
     }
 
 }
