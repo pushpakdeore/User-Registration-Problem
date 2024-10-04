77eeaea9821f4fa11cffba5d5b73b86b3e279624
pushpakdeore <deorepushpakk@gmail.com>
2024-10-04 15:14:27 +0530

    OK7

--- Validation.java
+++ Validation.java
@@ -80,7 +80,8 @@ public class Validation {
     }
 
     public static boolean isValidPassword(String password) {
-        return password.length() >= 8;
+        String regex = "^(?=.*[A-Z]).{8,}$";
+        return password.matches(regex);
     }
 
 }
