f6f5f891fb766c00f5cd71e3bfcf905dbba7141d
pushpakdeore <deorepushpakk@gmail.com>
2024-10-04 14:49:18 +0530

    OK1

--- Validation.java
+++ Validation.java
@@ -1,7 +1,23 @@
 package pushpak47UserRegistrationProblem;
-
+import java.util.Scanner;
 public class Validation {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        if (isValidFirstName(firstName)) {
+            System.out.println("Valid first name.");
+        } else {
+            System.out.println("Invalid first name. It must start with a capital letter and have at least 3 characters.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isValidFirstName(String firstName) {
+        String regex = "^[A-Z][a-z]{2,}$";
+        return firstName.matches(regex);
     }
 }
